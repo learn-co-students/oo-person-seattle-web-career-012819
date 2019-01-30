0c2c5420668f6c29c614d7b99aa409943b8a121b
ScottDenton <scottyd_14@hotmail.com>
2019-01-30 13:54:11 -0800

    Done

--- lib/person.rb
+++ lib/person.rb
@@ -1,97 +1,93 @@
 require 'pry'
 class Person
   attr_reader :name, :happiness, :hygiene
-  attr_accessor :bank
+  attr_accessor :bank_account
 
 
   def initialize(name)
     @name = name
-    @bank = 25
+    @bank_account = 25
     @happiness = 8
     @hygiene = 8
   end
 
+  def adjust
+    if @happiness >10
+      @happiness = 10
+    elsif @happiness < 0
+      @happiness = 0
+    end
+
+    if @hygiene >10
+      @hygiene = 10
+    elsif @hygiene < 0
+      @hygiene = 0
+    end
+  end
+
   def happiness= (happiness)
-    if happiness >= 10
-       @happiness = 10
-     elsif happiness <=0
-       @happiness = 0
-     else
-       @happiness = happiness
-     end
+      @happiness = happiness
+      self.adjust
   end
 
   def hygiene= (hygiene)
-
-     if hygiene >= 10
-        @hygiene = 10
-      elsif hygiene <=0
-        @hygiene = 0
-      else
-        @hygiene = hygiene
-      end
-
+    @hygiene = hygiene
+    self.adjust
   end
 
-def clean?
-  if @hygiene > 7
-    true
-  else
-    false
+  def clean?
+     @hygiene > 7
   end
-end
 
-def happy?
-   if @happiness > 7
-     true
-   else
-     false
-   end
-end
-
+  def happy?
+    @happiness > 7
+  end
 
 def get_paid(salary)
-  @bank += salary
+  @bank_account += salary
   "all about the benjamins"
 end
 
 def take_bath
-  if @hygiene >= 6
-    @hygiene = 10
-  else
   @hygiene +=4
-  end
+  self.adjust
   "♪ Rub-a-dub just relaxing in the tub ♫"
 end
 
 def work_out
-  if @happiness >= 8
-    @happiness = 10
-  else
-    @happiness +=2
-  end
-
-  if @hygiene <= 3
-    @hygiene = 0
-  else
-    @hygiene -=3
-  end
+  @happiness +=2
+  @hygiene -=3
+  self.adjust
   "♪ another one bites the dust ♫"
 end
 
 def call_friend(friend)
   @happiness +=3
-  friend.happiness = friend.happiness + 3
+  friend.happiness += 3
   "Hi #{friend.name}! It's #{self.name}. How are you?"
-  binding.pry
+  self.adjust
+  friend.adjust
+end
+
+def start_conversation(friend, topic)
+  if topic == 'politics'
+    self.happiness -= 2
+    friend.happiness -=2
+    "blah blah partisan blah lobbyist"
+  elsif topic == 'weather'
+    self.happiness += 1
+    friend.happiness +=1
+    "blah blah sun blah rain"
+  else
+    "blah blah blah blah blah"
+  end
+
 end
 
 
 
+# p1 = Person.new("Bob")
+# p2 = Person.new("Sarah")
 
-
-p1 = Person.new("Bob")
-p2 = Person.new("Sarah")
-binding.pry
 
 end
